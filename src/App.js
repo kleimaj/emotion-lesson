d8462f6b8b4a1e215fe7def17ed99adaa22aad1e
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2020-11-09 21:12:08 -0800

    adds theme object

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { SignUpModal } from './components';
 import { ThemeProvider } from 'emotion-theming';
 
 const Container = styled.div`
+  background: ${(props) => props.theme.colors.background};
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -14,18 +15,29 @@ const Container = styled.div`
 `;
 const theme = {
   colors: {
-    primary: '',
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: '#0cf',
+      },
+    },
   },
 };
 function App() {
   const [showModal, setShowModal] = useState(false);
   return (
-    <Container>
-      <SignUpModal showModal={showModal} setShowModal={setShowModal} />
-      <SecondaryButton onClick={() => setShowModal(!showModal)}>
-        Press me
-      </SecondaryButton>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+        <SecondaryButton onClick={() => setShowModal(!showModal)}>
+          Press me
+        </SecondaryButton>
+      </Container>
+    </ThemeProvider>
   );
 }
 
